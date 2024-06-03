514007ca54096d1171382e2327aec120436ab54f
Jaime de Ildefonso <jaimeildefonso@gmail.com>
2024-06-03 21:19:13 +0200

    Commit final del desarrollo de la aplicación. -Modificado el

--- NeuroTest/src/app/components/test/test.component.ts
+++ NeuroTest/src/app/components/test/test.component.ts
@@ -1,45 +1,34 @@
 import { HttpClient } from '@angular/common/http';
 import { Component } from '@angular/core';
-import { FormBuilder,FormGroup, Validators } from '@angular/forms';
-import { Observable } from 'rxjs';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-test',
   templateUrl: './test.component.html',
   styleUrls: ['./test.component.css']
 })
-
 export class TestComponent {
 
- test: FormGroup;
-  
-  constructor(private FormBuilder: FormBuilder, private http: HttpClient) {
+  test: FormGroup;
+
+  constructor(private formBuilder: FormBuilder, private http: HttpClient) {
+    this.test = this.formBuilder.group({
+      tiempoSesion: ['', [Validators.required, Validators.maxLength(2)]],
+      nivelEducativo: ['', Validators.required],
+      edadPaciente: ['', [Validators.required, Validators.maxLength(2)]],
+    });
+  }
+
+  enviar() {
+    const headers = { 'content-type': 'application/json' };
+    const body = JSON.stringify(this.test.value);
     
-      this.test = this.FormBuilder.group({
-        tiempoSesion: ['', Validators.required, Validators.maxLength(2)],
-        nivelEducativo: ['',[Validators.required]],
-        regiones: [''],
-        edadPaciente: ['', [Validators.required, Validators.maxLength(2)]],
+    this.http.post('http://localhost/tfg-fp/manejoDatos.php', body, { 'headers': headers })
+      .subscribe(response => {
+        console.log(response);
       });
   }
-  
-  enviar(){
-    console.log("hola");
-    //let body: any = {};
-    //body.tiempoSesion = this.test.value.tiempoSesion;
-    const headers = { 
-      'content-type': 'application/json'};
-
-    const body=JSON.stringify(this.test.value);
-    console.log(body);
-    this.http.post('http://localhost/tfg-fp/manejoDatos.php', body,{'headers':headers})
-      .subscribe((response)=>{
-        console.log(response)
-        //if (response.errors) {return errors} else {mostrar tabla con el array}
-      }); 
-  }
-  
-  
 }
+
 
 
--- neurotfg (2).sql
+++ neurotfg (2).sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 20-03-2024 a las 18:49:29
+-- Tiempo de generación: 03-06-2024 a las 19:09:37
 -- Versión del servidor: 10.4.32-MariaDB
 -- Versión de PHP: 8.2.12
 
@@ -30,7 +30,7 @@ SET time_zone = "+00:00";
 CREATE TABLE `funciones_cognitivas` (
   `categoria_id` int(11) NOT NULL,
   `categoria_nombre` varchar(50) DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_spanish_ci;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
 --
 -- Volcado de datos para la tabla `funciones_cognitivas`
@@ -40,7 +40,16 @@ INSERT INTO `funciones_cognitivas` (`categoria_id`, `categoria_nombre`) VALUES
 (1, 'Atención selectiva'),
 (2, 'Atención sostenida'),
 (3, 'atención alternante'),
-(4, 'Atención dividida');
+(4, 'Atención dividida'),
+(5, 'Memoria corto plazo'),
+(6, 'Memoria largo plazo'),
+(7, 'Memoria de trabajo'),
+(8, 'Memoria largo plazo'),
+(9, 'Lenguaje espontaneo'),
+(10, 'Fluencia'),
+(11, 'Denominacion'),
+(12, 'Comprension'),
+(13, 'Lectura y escritura');
 
 -- --------------------------------------------------------
 
@@ -58,14 +67,18 @@ CREATE TABLE `tests_cogni` (
   `regiones_relac` varchar(255) DEFAULT NULL,
   `tiempo_requerido` int(11) DEFAULT NULL,
   `edad_hasta` int(2) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_spanish_ci;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
 --
 -- Volcado de datos para la tabla `tests_cogni`
 --
 
 INSERT INTO `tests_cogni` (`test_id`, `test_nombre`, `categoria_id`, `descripcion`, `edad_desde`, `nivel_educativo`, `regiones_relac`, `tiempo_requerido`, `edad_hasta`) VALUES
-(1, 'Test D2', 1, 'Tarea de cancelación de elementos visuales. Evalúa capacidad de concentración mediante velocidad y precisión.', 6, '1', '', 8, 80);
+(1, 'Test D2', 1, 'Tarea de cancelación de elementos visuales. Evalúa capacidad de concentración mediante velocidad y precisión.', 6, '1', '', 8, 80),
+(2, 'TMT-A', 1, 'Test de lápiz y papel compuesto por una cantidad variable de números que el usuario ha de unir en orden creciente.', 9, '1', 'Tallo cerebral, diencéfalo, corteza cerebral.', 5, 99),
+(3, 'Symbol-Digit Modalities Test (SDMT)', 2, 'El objetivo es sustituir una serie de símbolos en dígitos a partir de una clave dada. La duración permite evaluar la pérdida de rendimiento en el tiempo.', 8, '1', NULL, 15, 99),
+(4, 'Paced Auditory Serial Addition Test (PASAT)', 4, 'Test auditivo que presenta un dígito al usuario en intervalos de 2 o 3\'\', con el objetivo de sumarlos mentalmente en serie.', 15, '2', NULL, 8, 80),
+(5, 'TMT-B', 3, 'Parte B del test TMT. En esta ocasión, el usuario ha de unir los elementos en orden ascendente, pero alternando en cada paso entre números y letras (A -> Z).', 9, '1', NULL, 8, 99);
 
 --
 -- Índices para tablas volcadas
